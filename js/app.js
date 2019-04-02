fd56b8d112f5daa1c859cc782cb3aea38d0c1c87
Levi Porter <levi@levibrooke.com>
2019-04-02 16:43:50 -0700

    Add render method for cookie totals

--- js/app.js
+++ js/app.js
@@ -16,19 +16,15 @@ function Store(location, min, max, avg) {
     }
   };
   this.cookieTotals = [];
+  allStores.push(this);
 }
 
-// Store locations
-new Store('1st and Pike', 23, 65, 6.3);
-new Store('Seatac Airport', 3, 24, 1.2);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('Capitol Hill', 20, 38, 2.3);
-new Store('Alki', 2, 16, 4.6);
+var allStores = [];
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 
 // Render table header
-function makeTable() {
+function makeTableHeader() {
   var row = document.createElement('tr');
   var emptyHeader = document.createElement('th');
   emptyHeader.textContent = '';
@@ -48,8 +44,48 @@ function makeTable() {
   table.appendChild(row);
 }
 
-makeTable();
+// Render table footer
+function makeTableFooter() {
 
+}
+
+// Render cookie sales
+Store.prototype.render = function() {
+  var row = document.createElement('tr');
+
+  var storeLabel = document.createElement('td');
+  storeLabel.textContent = this.location;
+  row.appendChild(storeLabel);
+
+  for (let i = 0; i < this.cookieTotals.length; i++) {
+    var entry = document.createElement('td');
+    entry.textContent = this.cookieTotals[i];
+    row.appendChild(entry);
+  }
+
+  var table = document.getElementById('table');
+  table.appendChild(row);
+};
+
+// Store locations
+new Store('1st and Pike', 23, 65, 6.3);
+new Store('Seatac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
+
+function renderAllCookies() {
+  for (let i = 0; i < allStores.length; i++) {
+    allStores[i].calculateCookies(hours);
+    allStores[i].render();
+    console.log('allStores[i].cookieTotals', allStores[i].cookieTotals);
+  }
+}
+
+// Invoke
+makeTableHeader();
+console.log('allStores', allStores);
+renderAllCookies();
 
 // Generate sales reports page
 function createReport(list, store) {
